8a3c0417cf67bb30be4ec51ec2f899260385fd25
Jafet Carrasco <jafet.carrasco@tecsup.edu.pe>
2022-06-28 20:57:56 -0500

    Add project files.

---
+++ WEBSiteTecsup/Controllers/MusicasController.cs
@@ -0,0 +1,153 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WEBSiteTecsup.Data;
+using WEBSiteTecsup.Models;
+
+namespace WEBSiteTecsup.Controllers
+{
+    public class MusicasController : Controller
+    {
+        private WEBSiteTecsupContext db = new WEBSiteTecsupContext();
+
+        // GET: Musicas
+        public async Task<ActionResult> Index()
+        {
+            List<Musica> model = new List<Musica>();
+            var client = new HttpClient();
+            var urlBase = "https://localhost:44315/";
+            client.BaseAddress = new Uri(urlBase);
+
+            var url = string.Concat(urlBase, "/Api", "/Musicas", "/GetMusicas");
+            var response = client.GetAsync(url).Result;
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                //Json to Response
+                var musicas = JsonConvert.DeserializeObject<List<Musica>>(result);
+                model = musicas;
+            }
+            return View(model);
+        }
+
+        // GET: Musicas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Musica musica = db.Musicas.Find(id);
+            if (musica == null)
+            {
+                return HttpNotFound();
+            }
+            return View(musica);
+        }
+
+        // GET: Musicas/Create
+        [HttpPost]
+        public async Task<ActionResult> Create(Musica model)
+        {
+            try
+            {
+                //Class a JSON
+                var request = JsonConvert.SerializeObject(model);
+                var content = new StringContent(request, Encoding.UTF8, "application/json");
+
+                var client = new HttpClient();
+                var urlBase = "https://localhost:44315";
+                client.BaseAddress = new Uri(urlBase);
+                var url = string.Concat(urlBase, "/Api", "/Musicas","/PostMusica");
+
+                var response = client.PostAsync(url, content).Result;
+
+                if (response.StatusCode == HttpStatusCode.Created)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        // GET: Musicas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Musica musica = db.Musicas.Find(id);
+            if (musica == null)
+            {
+                return HttpNotFound();
+            }
+            return View(musica);
+        }
+
+        // POST: Musicas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MusicID,MusicName,MusicAutor")] Musica musica)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(musica).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(musica);
+        }
+
+        // GET: Musicas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Musica musica = db.Musicas.Find(id);
+            if (musica == null)
+            {
+                return HttpNotFound();
+            }
+            return View(musica);
+        }
+
+        // POST: Musicas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Musica musica = db.Musicas.Find(id);
+            db.Musicas.Remove(musica);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WEBSiteTecsup/Models/Musica.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WEBSiteTecsup.Models
+{
+    public class Musica
+    {
+        [Key]
+        public int MusicID { get; set; }
+
+        public string MusicName { get; set; }
+
+        public string MusicAutor { get; set; }
+
+    }
+}
